e04e47b3cb18c0e3ade561205bc8903d1a4cfd7a
MernStackExpert <mdnirob30k@gmail.com>
2025-11-08 19:22:38 +0600

    Add Navbar

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router';
 
 const Root = () => {
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
+      <header>
+        <Navbar></Navbar>
+      </header>
+
+      <main className='flex-grow mt-50'>
+        <Outlet></Outlet>
+      </main>
+
+      <footer className='bg-red-300'>
+        h1 this is footer
+      </footer>
       
     </div>
   );
