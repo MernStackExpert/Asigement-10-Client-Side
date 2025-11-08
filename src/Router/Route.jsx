073164cc5ef00724df5f9836326c6cb27e81aac4
MernStackExpert <mdnirob30k@gmail.com>
2025-11-08 19:09:31 +0600

    first commit

---
+++ src/Router/Route.jsx
@@ -0,0 +1,20 @@
+
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import Root from "../Layouts/Root";
+
+export const router = createBrowserRouter([
+  {
+    path:"/",
+    Component:Root,
+    children:[
+      {
+        index:true,
+        Component:Home
+      },
+      {
+        
+      }
+    ]
+  }
+])
